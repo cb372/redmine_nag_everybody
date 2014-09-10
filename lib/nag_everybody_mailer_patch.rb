5b15877b1715a58ee02244b80ca2601825721035
Chris Birchall <c-birchall@m3.com>
2014-09-10 12:16:14 +0900

    Implement reminder mail logic, new mail templates

--- lib/nag_everybody_mailer_patch.rb
+++ lib/nag_everybody_mailer_patch.rb
@@ -1,5 +1,7 @@
 module NagEverybodyMailerPatch
   def self.included(base) # :nodoc:
+
+    # Add class methods
     base.instance_eval do
 
       # Sends reminders to both issue assignees and issue watchers.
@@ -18,14 +20,14 @@ module NagEverybodyMailerPatch
         tracker = options[:tracker] ? Tracker.find(options[:tracker]) : nil
         user_ids = options[:users]
 
-        scope = Issue.open.where("#{Issue.table_name}.assigned_to_id IS NOT NULL" +
+        by_assignee_scope = Issue.open.where("#{Issue.table_name}.assigned_to_id IS NOT NULL" +
           " AND #{Project.table_name}.status = #{Project::STATUS_ACTIVE}" +
           " AND #{Issue.table_name}.due_date <= ?", days.day.from_now.to_date
         )
-        scope = scope.where(:assigned_to_id => user_ids) if user_ids.present?
-        scope = scope.where(:project_id => project.id) if project
-        scope = scope.where(:tracker_id => tracker.id) if tracker
-        issues_by_assignee = scope.includes(:status, :assigned_to, :project, :tracker).
+        by_assignee_scope = by_assignee_scope.where(:assigned_to_id => user_ids) if user_ids.present?
+        by_assignee_scope = by_assignee_scope.where(:project_id => project.id) if project
+        by_assignee_scope = by_assignee_scope.where(:tracker_id => tracker.id) if tracker
+        issues_by_assignee = by_assignee_scope.includes(:status, :assigned_to, :project, :tracker).
                                   group_by(&:assigned_to)
         issues_by_assignee.keys.each do |assignee|
           if assignee.is_a?(Group)
@@ -36,62 +38,66 @@ module NagEverybodyMailerPatch
           end
         end
 
-        issues_by_assignee.each do |assignee, issues|
-          reminder(assignee, issues, days).deliver if assignee.is_a?(User) && assignee.active?
+        by_watcher_scope = Issue.open.where(
+          "#{Project.table_name}.status = #{Project::STATUS_ACTIVE}" +
+          " AND #{Issue.table_name}.due_date <= ?", days.day.from_now.to_date
+        )
+        by_watcher_scope = by_watcher_scope.where("EXISTS(SELECT 1 FROM watchers WHERE watchable_id = issues.id AND watchable_type = 'Issue' AND user_id IN (?))", user_ids) if user_ids.present?
+        by_watcher_scope = by_watcher_scope.where(:project_id => project.id) if project
+        by_watcher_scope = by_watcher_scope.where(:tracker_id => tracker.id) if tracker
+        issues = by_watcher_scope.includes(:status, :watchers, :project, :tracker)
+        issues_by_watcher = {}
+        issues.each do |issue|
+          issue.watchers.each do |watcher|
+            user = User.find_by_id(watcher.user_id)
+            issues_by_watcher[user] ||= []
+            issues_by_watcher[user] << issue
+          end
+        end
+
+        # TODO Remove any issues for projects that don't have this plugin enabled
+
+        # combine issues_by_watcher and issues_by_assignee
+        issues_by_user = {}
+        (issues_by_assignee.keys + issues_by_watcher.keys).to_set.each do |user|
+          assigned = issues_by_assignee[user] || []
+          watching = issues_by_watcher[user] || []
+
+          # Filter out any duplicates 
+          # (i.e. issues that the user is both assigned to and watching)
+          watching = watching.select{ |i| !assigned.include? i }
+          issues_by_user[user] = { :assigned => assigned, :watching => watching }
+        end
+
+        issues_by_user.each do |user, issues|
+          extended_reminder(user, issues[:assigned], issues[:watching], days).deliver if user.is_a?(User) && user.active?
         end
       end
 
     end
+
+    # Add instance methods
+    base.class_eval do
+
+      # Send a reminder email including both assigned and watched issues
+      def extended_reminder(user, assigned_issues, watched_issues, days)
+        puts "Sending reminder mail to #{user.mail}"
+        set_language_if_valid user.language
+        @assigned_issues = assigned_issues
+        @watched_issues = watched_issues
+        @days = days
+        @assigned_issues_url = url_for(:controller => 'issues', :action => 'index',
+                                    :set_filter => 1, :assigned_to_id => user.id,
+                                    :sort => 'due_date:asc')
+        @watched_issues_url = url_for(:controller => 'issues', :action => 'index',
+                                    :set_filter => 1, :watcher_id => user.id,
+                                    :sort => 'due_date:asc')
+        mail :to => user.mail,
+          :subject => l(:mail_subject_reminder, :count => assigned_issues.size + watched_issues.size, :days => days)
+      end
+
+    end
+
   end
 
-
-    #def issue_add_with_fine_grain_settings(issue, to_users, cc_users)
-      #project = issue.project
-      #issue_mail = issue_add_without_fine_grain_settings(issue, to_users, cc_users)
-      ## Make sure the plugin is still enabled
-      #unless EnabledModule.find_by_project_id_and_name(project,"mail_options")
-        #return issue_mail
-      #end
-
-      #mo = MailOptions.find_by_project_id(project)
-
-      ## If turned off, blank the headers
-      #unless (mo.nil? or mo.send_on_create)
-        #blank_mail
-      #end
-
-      ##And return the original value
-      #return issue_mail
-    #end
-
-    #def issue_edit_with_fine_grain_settings(journal, to_users, cc_users)
-      #project = journal.journalized.reload.project
-      #issue_mail = issue_edit_without_fine_grain_settings(journal, to_users, cc_users)
-
-      ## Make sure the plugin is enabled before doing anything
-      #unless EnabledModule.find_by_project_id_and_name(project,"mail_options")
-        #return issue_mail
-      #end
-
-      #mo = MailOptions.find_by_project_id(project)
-
-      #unless mo # If there isn't one just do the usual
-        #return issue_mail
-      #end
-
-      #if journal.notify? &&
-        #(
-        #(journal.notes.present? && mo.send_on_comment) || 
-        #(journal.new_value_for('assigned_to_id').present? && mo.send_on_assignee_change) ||
-        #(journal.new_value_for('subject').present? && mo.send_on_subject_change) ||
-        #(journal.new_value_for('attachment').present? && mo.send_on_file_upload) ||
-        #(journal.new_value_for('tracker_id').present? && mo.send_on_tracker_change)
-      #)
-        #return issue_mail
-      #end
-      ## No reason to notice found, disable mail
-      #blank_mail
-      #return issue_mail
-    #end
- 
 end
